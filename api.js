51b647abd5df09d2ee3ca0a5e62bdc8f0addda04
Matt Robertson <mattrobertson1414@gmail.com>
2019-09-05 04:05:17 +0000

    added basic server

---
+++ api.js
@@ -0,0 +1,37 @@
+var router = require("express-promise-router")()
+
+let currentLight = 'off'
+
+router.post('/redon', async (req, res, next) => {
+  console.log(`turn the red light on from ${currentLight}!`)
+
+  res.json(`Turning red light on from ${currentLight}!`)
+
+  currentLight = 'red'
+})
+
+router.post('/yellowon', async (req, res, next) => {
+  console.log(`turn the yellow light on from ${currentLight}!`)
+
+  res.json(`Turning yellow light on from ${currentLight}!`)
+
+  currentLight = 'yellow'
+})
+
+router.post('/greenon', async (req, res, next) => {
+  console.log(`turn the green light on from ${currentLight}!`)
+
+  res.json(`Turning green light on from ${currentLight}!`)
+
+  currentLight = 'green'
+})
+
+router.post('/off', async (req, res, next) => {
+  console.log(`The ${currentLight}! is turned off`)
+
+  res.json(`Turning ${currentLight} off!`)
+
+  currentLight = 'off'
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+var http = require('http');
+var express = require('express');
+var apiRoute = require('./api');
+
+var app = express();
+
+var port = normalizePort(process.env.PORT || '8080');
+app.set('port', port);
+
+app.use('/api', apiRoute);
+app.use('/', express.static('src'))
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError)
+server.on('listening', onListening);
+
+function onError(error) {
+  console.error('HTTP Server Error' + error)
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
+

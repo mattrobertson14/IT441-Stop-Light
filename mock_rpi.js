426125c621009f201a02bd8479a05a2f3cb790e9
Matthew Robertson <mattrobertson1414@gmail.com>
2019-09-06 08:11:01 -0600

    added new functions to mock rpi

--- mock_rpi.js
+++ mock_rpi.js
@@ -1,37 +1,64 @@
 let rpi = {}
 
- let currentLight = 'off'
+let currentLight = 'off'
 
- rpi.getCurrentLight = () => {
+rpi.getCurrentLight = () => {
   return {light: currentLight}
 }
 
- rpi.redOn = () => {
+rpi.redOn = () => {
   console.log('Turning Red On')
-  currentLight = 'red'
+  
+  changeLight('red')
 
    return {light: currentLight}
 }
 
- rpi.yellowOn = () => {
+rpi.yellowOn = () => {
   console.log('Turning Yellow On')
-  currentLight = 'yellow'
+  
+  changeLight('yellow')
 
-   return {light: currentLight}
+  return {light: currentLight}
 }
 
- rpi.greenOn = () => {
+rpi.greenOn = () => {
   console.log('Turning Green On')
-  currentLight = 'green'
+  
+  changeLight('green')
 
-   return {light: currentLight}
+  return {light: currentLight}
 }
 
- rpi.lightsOut = () => {
+rpi.lightsOut = () => {
   console.log('Turning Lights Off')
-  currentLight = 'off'
+  
+  changeLight('off')
 
-   return {light: currentLight}
+  return {light: currentLight}
+}
+
+rpi.cycle = ()  => {
+  console.log('Turning on light cycling')
+
+  cycle()
+
+  return {light: currentLight()}
+}
+
+const changeLight = (color) => {
+  if (cyclingTimeout) clearTimeout(cyclingTimeout)
+  if (color === 'red') currentLight = 'red'
+  else if (color === 'yellow') currentLight = 'yellow'
+  else if (color === 'green') currentLight = 'green'
+  else currentLight = 'off'
+}
+
+const cycle = () => {
+  changeLight('red')
+  setTimeout(changeLight('yellow'), 5000)
+  setTimeout(changeLight('green'), 7000)
+  cyclingTimeout = setTimeout(cycle, 12000)
 }
 
  module.exports = rpi 

4a24af1a35b9dba84766876b53b1f7610c190412
Matthew Robertson <mattrobertson1414@gmail.com>
2019-09-08 16:39:14 -0600

    set up proxy and set up basic frontend to hit api endpoints

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [light, setLight] = useState('off')
+
+  const lights = ['red', 'yellow', 'green']
+
+  useEffect(() => {
+    axios.get('/api/status').then(res => {
+      setLight(res.data.light)
+    }).catch(console.log)
+  }, [])
+
+  const changeLight = (color) => {
+    axios.post(`/api/${color}on`).then(res => {
+      setLight(res.data.light)
+    }).catch(console.log)
+  }
+
+  const turnOffLight = () => {
+    axios.post('/api/off').then(res => {
+      setLight(res.data.light)
+    }).catch(console.log)
+  }
+
   return (
     <div className="App">
-      Hello World
+      <h1>Light: {light}</h1>
+      <div className='stoplight'>
+        {lights.map(l => (
+          <div 
+          className={`light ${l}${l === light? ' on':''}`} 
+          onClick={() => changeLight(l)}
+        ></div>
+      ))}
+      </div>
+      <button onClick={() => turnOffLight()}>TURN OFF</button>
     </div>
   );
 }
